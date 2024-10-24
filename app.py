f793487793504ba15cc76d6deec7872c7ba328a1
Josh Duncanson <107645988+josiahuriah@users.noreply.github.com>
2024-10-24 20:29:04 +0000

    Developed functionality and added styling

--- app.py
+++ app.py
@@ -1,10 +1,38 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Bimini, Bahamas',
+    'salary': 'BSD $68,000.00'
+  },
+  {
+    'id': 2,
+    'title': 'Front End Engineer',
+    'location': 'Remote',
+    'salary': 'BSD $48,000.00'
+  },
+  {
+    'id': 3,
+    'title': 'Data Analyst',
+    'location': 'Exuma, Bahamas',
+    'salary': 'BSD $64,000.00'
+  },
+  {
+    'id': 4,
+    'title': 'Back End Engineer',
+    'location': 'Remote',
+  }
+]
+
 @app.route("/")
 def hello_world():
-  return "Hello, world"
+  return render_template("home.html",
+                         jobs=JOBS,
+                        company_name = "Josiah")
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
